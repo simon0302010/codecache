760242ed64a37dd6fdd932eebade901c078061bb
simon0302010 <simon0302010@gmail.com>
2025-12-02 12:37:52 +0100

    added a scrollbar

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -2,20 +2,24 @@ use crossterm::event::{self, Event, KeyCode, KeyEventKind};
 use ratatui::{
     Frame,
     layout::{
-        Constraint::{Fill, Length, Min},
-        Layout,
+        Constraint::{Length, Min},
+        Layout, Margin,
     },
     style::{Style, Stylize},
-    widgets::Block,
+    widgets::{Block, Borders, List, Scrollbar, ScrollbarOrientation, ScrollbarState},
 };
 
 pub struct CodeCache {
     running: bool,
+    scroll_position: usize,
 }
 
 impl CodeCache {
     pub fn new() -> Self {
-        CodeCache { running: true }
+        CodeCache {
+            running: true,
+            scroll_position: 0,
+        }
     }
 
     pub fn run(&mut self) {
@@ -32,24 +36,44 @@ impl CodeCache {
     fn draw(&self, frame: &mut Frame) {
         let vertical = Layout::vertical([Length(1), Min(0), Length(1)]);
         let [title_area, main_area, status_area] = vertical.areas(frame.area());
-        let horizontal = Layout::horizontal([Fill(1); 2]);
-        let [left_area, right_area] = horizontal.areas(main_area);
+        // let horizontal = Layout::horizontal([Fill(1); 2]);
+        // let [left_area, right_area] = horizontal.areas(main_area);
+
+        // define code snippets
+        let snippets = (1..=100)
+            .map(|i| format!("Item {}", i))
+            .collect::<Vec<String>>();
+
+        let scrollbar = Scrollbar::new(ScrollbarOrientation::VerticalRight)
+            .begin_symbol(Some("↑"))
+            .end_symbol(Some("↓"));
+
+        let mut scrollbar_state =
+            ScrollbarState::new(snippets.len()).position(self.scroll_position);
 
         frame.render_widget(
             Block::new()
-                .title("CodeCache")
+                .title(format!("CodeCache v{}", env!("CARGO_PKG_VERSION")))
                 .title_alignment(ratatui::layout::Alignment::Center)
                 .title_style(Style::new().red().bold()),
             title_area,
         );
         frame.render_widget(
             Block::new()
-                .title("0 Snippets saved")
+                .title(format!("{} snippets in storage", snippets.len()))
                 .title_alignment(ratatui::layout::Alignment::Center),
             status_area,
         );
-        frame.render_widget(Block::bordered().title("Left"), left_area);
-        frame.render_widget(Block::bordered().title("Right"), right_area);
+        frame.render_widget(
+            List::new(snippets).block(
+                Block::new()
+                    .borders(Borders::TOP | Borders::BOTTOM)
+                    .title("Snippets")
+                    .title_alignment(ratatui::layout::Alignment::Center),
+            ),
+            main_area,
+        );
+        frame.render_stateful_widget(scrollbar, main_area.inner(Margin { vertical: 1, horizontal: 0 }), &mut scrollbar_state);
     }
 
     fn handle_events(&mut self) {
