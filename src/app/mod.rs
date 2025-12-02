853fdf6e8ffd358fc4d015bfd38fa248d25a5ea6
simon0302010 <simon0302010@gmail.com>
2025-12-02 10:06:54 +0100

    improved code structure

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -1,24 +1,22 @@
 use crossterm::event::{self, Event, KeyCode, KeyEventKind};
 use ratatui::{Frame, text::Text};
 
-pub struct CodeCache {}
+pub struct CodeCache {
+    running: bool
+}
 
 impl CodeCache {
     pub fn new() -> Self {
-        CodeCache {}
+        CodeCache { running: true }
     }
 
     pub fn run(&mut self) {
         let mut terminal = ratatui::init();
-        loop {
-            terminal.draw(|frame| self.draw(frame)).expect("failed to draw frame");
-            match event::read().expect("failed to read event") {
-                Event::Key(key) if key.kind == KeyEventKind::Press => match key.code {
-                    KeyCode::Char('q') => break,
-                    _ => {}
-                } ,
-                _ => {}
-            }
+        while self.running {
+            terminal
+                .draw(|frame| self.draw(frame))
+                .expect("failed to draw frame");
+            self.handle_events();
         }
         ratatui::restore();
     }
@@ -26,5 +24,15 @@ impl CodeCache {
     fn draw(&self, frame: &mut Frame) {
         let text = Text::raw("Hello World!");
         frame.render_widget(text, frame.area());
+    }
+
+    fn handle_events(&mut self) {
+        match event::read().expect("failed to read event") {
+            Event::Key(key) if key.kind == KeyEventKind::Press => match key.code {
+                KeyCode::Char('q') => self.running = false,
+                _ => {}
+            },
+            _ => {}
+        }
     }
 }
